81e7f7c27054295cbd1adbfbc5e8aea85afcbb79
igorcoding <igorcoding@gmail.com>
2016-04-29 15:03:17 +0300

    some changes

--- RequestSysApplication/prototype.py
+++ RequestSysApplication/prototype.py
@@ -4,10 +4,7 @@ from RequestSysApplication.models import MyRequest, Department
 class Prototype:
     def __init__(self):
         self._objects = {}
-
-
-
-    
+        self.igor_was_here = True
 
     def clone(self, name, **attr):
         """Clone a registered object and update inner attributes dictionary"""
@@ -20,3 +17,10 @@ class Prototype:
         obj.__dict__.update(attr)
         return obj
 
+    def was_igor_here():
+        return hasattr(self, 'igor_was_here') and self.igor_was_here
+
+
+if __name__ == "__main__":
+    p = Prototype()
+    print(p.was_igor_here())
